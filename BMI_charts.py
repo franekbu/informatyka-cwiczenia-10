79c8b17da122fb71749194dedb52c4af640ff4ae
Franek Bury <fbury2006@gmail.com>
2026-01-05 21:24:35 +0100

    3 jednoczesnie, oraz strefy bmi w scatter

--- BMI_charts.py
+++ BMI_charts.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import numpy as np
 
 
 pallet = sns.diverging_palette(250, 30, l=65, center="dark", as_cmap=True)
@@ -29,32 +30,89 @@ def get_data(file_name: str) -> pd.DataFrame:
     new_df = pd.concat([bmi_data, bmi_cat], axis=1)
     return new_df
 
+def add_bmi_zones(axe, min_height: int, max_height: int) -> None:
+    height_line = np.linspace(min_height, max_height, 100)
 
-# Create a visualization
-def get_replot_plot(data: pd.DataFrame) -> None:
-    sns.relplot(
+    bmi_zones: list[tuple[float, float, str]] = [
+        (0, 18.5, 'yellow'),
+        (18.5, 25, 'green'),
+        (25, 30, 'yellow'),
+        (30, 35, 'orange'),
+        (35, 40, 'orange'),
+        (40, 80, 'red'),
+    ]
+    for zone in bmi_zones:
+
+        # retransform bmi formula 
+        min_zone_weights = zone[0] * pow((height_line / 100), 2)
+        max_zone_weights = zone[1] * pow((height_line / 100), 2)
+
+        axe.plot(height_line, min_zone_weights, color=zone[2], linestyle='--', alpha=0.5)
+        axe.plot(height_line, max_zone_weights, color=zone[2], linestyle='--', alpha=0.5)
+        axe.fill_between(height_line, min_zone_weights, max_zone_weights, color=zone[2], alpha=0.15)
+    
+    axe.set_ylim(bottom=30, top=150)
+
+def prepare_scatter_plot(data: pd.DataFrame, axe) -> None:
+    add_bmi_zones(
+        axe=axe,
+        min_height=data['wzrost[cm]'].min(),
+        max_height=data['wzrost[cm]'].max(),
+    )
+    sns.scatterplot(
         data=data,
         x="wzrost[cm]", 
         y="waga[kg]",
         hue="bmi",
-        palette= pallet,
-        size="bmi", sizes=(15, 200)
+        palette=pallet,
+        # facecolor='grey',
+        edgecolor='black',
+        size="bmi",
+        sizes=(15, 200),
+        ax=axe
     )
-    plt.show()
 
-def get_displot_plot(data):
-    sns.displot(
+    axe.set_title("Relacja waga vs wzrost")
+
+
+def prepare_histplot_plot(data: pd.DataFrame, axe):
+    sns.histplot(
         data=data,
         x='bmi_category',
-        palette=pallet,
         hue='bmi_category',
+        palette=pallet,
+        discrete=True,
+        shrink=0.8,
+        legend=False,
+        ax=axe
     )
+    axe.set_title("Liczebność kategorii BMI")
+
+
+def prepare_box_plot(data: pd.DataFrame, axe) -> None:
+    sns.boxplot(
+        data=data,
+        x='bmi_category',
+        y='waga[kg]',
+        hue='bmi_category',
+        palette=pallet, #type: ignore
+        ax=axe
+    )
+    axe.set_title("Rozkład wagi w kategoriach BMI")
+
+def combined_plots(data: pd.DataFrame) -> None:
+    fig, axes = plt.subplots(1, 3, figsize=(18,6))
+
+    prepare_scatter_plot(data=data, axe=axes[0])
+    prepare_histplot_plot(data=data, axe=axes[1])
+    prepare_box_plot(data=data, axe=axes[2])
+
+    plt.tight_layout()
     plt.show()
 
 def main() -> None:
     data = get_data('wynik_bmi.txt')
-    get_replot_plot(data=data)
-    get_displot_plot(data=data)
+    combined_plots(data=data)
 
 
 if __name__ == '__main__':
